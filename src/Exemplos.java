5e2e0488c429de222d51ef4171d20d1cad8d1417
TabataGrigaitis <tabatagrigaitis@gmail.com>
2020-03-04 20:38:00 -0300

    Exemplos de metodo

--- src/Exemplos.java
+++ src/Exemplos.java
@@ -1,12 +1,21 @@
 
 public class Exemplos {
-	// [ctrl]+[espaço] completa
-	// [crtl]+[shift]+[F] identa
-	// crtl+f11 executa
+	// [ctrl]+[espaço]       completa
+	// [crtl]+[shift]+[F]    identa
+	// [crtl]+[f11]          executa
 	public static void main(String[] args) {
 
-		int idade = 26;
+		//int idade = 30;
+		//ehMaiorIdade(19);
+		//ehMaiorIdade(12);
+		//ehMaiorIdade(idade);
+		classificar(-90);
 
+	}
+	//public = todos acessam  
+	//static = ?
+	//void = não retorna
+	public static void ehMaiorIdade(int idade) {
 		if (idade >= 18) {
 			// syso = system.out.printIn
 			System.out.println("maior de idade");
@@ -14,4 +23,22 @@ public class Exemplos {
 			System.out.println("menor de idade");
 		}
 	}
+	public static void classificar(int num) {
+		//negativo,positivo,zero
+		if(num>0) {
+			System.out.println("positivo");
+		}
+		else {
+			if (num<0) {
+				System.out.println("negativo");
+			} else {
+				System.out.println("zero");
+			}
+		}
+		
+	}
+	
+	
+	
+	
 }
