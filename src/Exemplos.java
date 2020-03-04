7d306285d910168aaf3750b289469a383153e2fa
TabataGrigaitis <tabatagrigaitis@gmail.com>
2020-03-04 20:00:55 -0300

    revisão de if

---
+++ src/Exemplos.java
@@ -0,0 +1,17 @@
+
+public class Exemplos {
+	// [ctrl]+[espaço] completa
+	// [crtl]+[shift]+[F] identa
+	// crtl+f11 executa
+	public static void main(String[] args) {
+
+		int idade = 26;
+
+		if (idade >= 18) {
+			// syso = system.out.printIn
+			System.out.println("maior de idade");
+		} else {
+			System.out.println("menor de idade");
+		}
+	}
+}
